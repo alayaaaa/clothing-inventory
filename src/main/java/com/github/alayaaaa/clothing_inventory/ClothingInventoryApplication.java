4012491a13794bfab971383a3e8afb688d54172e
unknown <marionjulai.flores@gmail.com>
2026-06-15 19:28:58 -0400

    Initial commit

---
+++ src/main/java/com/github/alayaaaa/clothing_inventory/ClothingInventoryApplication.java
@@ -0,0 +1,13 @@
+package com.github.alayaaaa.clothing_inventory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClothingInventoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClothingInventoryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/alayaaaa/clothing_inventory/ClothingInventoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.alayaaaa.clothing_inventory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClothingInventoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
